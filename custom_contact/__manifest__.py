11cddf0f9ab86598ffea796c3e8ba5ad5bf9017c
odoo <you@example.com>
2025-06-18 19:31:18 +0530

    [ADD] custom_contact : create new module for contact inheritance and show vendor create option only in contact menu rest invisible

---
+++ custom_contact/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "custom_contact",
+
+    'summary': "Custom Contact",
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'contacts', 'account','accountant', 'purchase'],
+
+    # always loaded
+    'data': [
+        #'security/res_group.xml',
+        #'views/custom_partner.xml',
+    ],
+
+}
