1a9613593e43bc3a3fce126236bc39121d5a316b
odoo <you@example.com>
2025-07-03 17:37:19 +0530

    [ADD] custom_contact : make fields mandatory false

--- custom_contact/wizard/vendor_kyc.py
+++ custom_contact/wizard/vendor_kyc.py
@@ -9,33 +9,6 @@ class VendorKycWizard(models.TransientModel):
     _name = 'vendor.kyc.wizard'
     _description = 'Vendor KYC Wizard'
 
-    # @api.onchange('additional_phone', 'director_phone')
-    # def _onchange_phone_numbers(self):
-    #     phone_pattern = re.compile(r'^\+?[0-9]{7,15}$')
-    #     for field_label, number in [('Additional Contact Number', self.additional_phone),
-    #                                 ('Director Contact Number', self.director_phone)]:
-    #         if number and not phone_pattern.match(number):
-    #             return {
-    #                 'warning': {
-    #                     'title': "Invalid Phone Number",
-    #                     'message': f"{field_label} must be a valid phone number (e.g., +911234567890 or 1234567890)."
-    #                 }
-    #             }
-
-    # @api.onchange('email', 'additional_email', 'director_email')
-    # def _onchange_email_format(self):
-    #     email_pattern = re.compile(r'^[\w\.-]+@[\w\.-]+\.\w{2,}$')
-    #     for label, email in [('Email', self.email),
-    #                          ('Additional Email', self.additional_email),
-    #                          ('Director Email', self.director_email)]:
-    #         if email and not email_pattern.match(email):
-    #             return {
-    #                 'warning': {
-    #                     'title': "Invalid Email Format",
-    #                     'message': f"{label} must be a valid email address (e.g., user@example.com)."
-    #                 }
-    #             }
-
     @api.constrains('additional_phone', 'director_phone')
     def _check_phone_numbers(self):
         phone_pattern = re.compile(r'^\+?[0-9]{10,14}$')  # Accepts optional '+' and 7–15 digits
@@ -57,16 +30,16 @@ class VendorKycWizard(models.TransientModel):
 
     partner_id = fields.Many2one('res.partner', string='Contact', domain="[('id', '=', active_id)]")
     email = fields.Char("Email", required=True)
-    point_of_contact = fields.Char("Point of Contact", required=True)
-    business_legal_name = fields.Char("Business Legal Name", required=True)
-    business_trade_name = fields.Char("Business Trade Name", required=True)
-    business_street = fields.Char("Address", required=True)
-    business_city = fields.Char("City", required=True)
-    business_pincode = fields.Char("Pincode", required=True)
+    point_of_contact = fields.Char("Point of Contact", required=False)
+    business_legal_name = fields.Char("Business Legal Name", required=False)
+    business_trade_name = fields.Char("Business Trade Name", required=False)
+    business_street = fields.Char("Address", required=False)
+    business_city = fields.Char("City", required=False)
+    business_pincode = fields.Char("Pincode", required=False)
     additional_street = fields.Char("Address")
     additional_city = fields.Char("City")
     additional_zip = fields.Char("Pincode")
-    additional_phone = fields.Char("Contact Number", required=True)
+    additional_phone = fields.Char("Contact Number", required=False)
     additional_email = fields.Char("Email Address")
     const_business = fields.Selection([('Sole Proprietor', 'Sole Proprietor'),
                                        ('Partnership', 'Partnership'),
@@ -74,40 +47,40 @@ class VendorKycWizard(models.TransientModel):
                                        ('LLP', 'LLP'),
                                        ('HUF(Karta)', 'HUF(Karta)'),
                                        ('Other', 'Other'),
-                                       ], string="Constitution of Business", required=True)
+                                       ], string="Constitution of Business", required=False)
     # const_business = fields.Many2one('constitution.business', string="Constitution of Business", required=True)
     other_business = fields.Char("If Other, Specify?")
     # no_partner_director = fields.Many2one('number.partner.director', string="Number of Managing Partner / Directors")
-    director_name = fields.Char(string="Name of the Owner / Director", required=True)
-    director_phone = fields.Char(string="Contact Number", required=True)
-    director_email = fields.Char(string="Email Address", required=True)
-    aadhaar_card = fields.Binary(string="Aadhaar Card", required=True)
-    pan_card = fields.Binary(string="PAN Card (Proprietor)", required=True)
-    gst_no = fields.Char(string="GST Number", required=True)
+    director_name = fields.Char(string="Name of the Owner / Director", required=False)
+    director_phone = fields.Char(string="Contact Number", required=False)
+    director_email = fields.Char(string="Email Address", required=False)
+    aadhaar_card = fields.Binary(string="Aadhaar Card", required=False)
+    pan_card = fields.Binary(string="PAN Card (Proprietor)", required=False)
+    gst_no = fields.Char(string="GST Number", required=False)
     udyam_number = fields.Char(string="Udyam Certificate Number")
     gst_certificate = fields.Many2many('ir.attachment', 'vendor_kyc_gst_cert_rel', 'wizard_id', 'attachment_id',
-                                       string="Company GST Certificate", required=True)
+                                       string="Company GST Certificate", required=False)
 
     udyam_document = fields.Many2many('ir.attachment', 'vendor_kyc_shop_documents_rel', 'wizard_id', 'attachment_id',
-                                      string="Shop Act documents / Udyam Documents", required=True)
+                                      string="Shop Act documents / Udyam Documents", required=False)
 
     gst_return_duration = fields.Selection([('Monthly', 'Monthly'), ('Quarterly', 'Quarterly')],
-                                           required=True, string="GST Return duration")
+                                           required=False, string="GST Return duration")
 
     shop_photos = fields.Many2many('ir.attachment', 'vendor_kyc_shop_photos_rel', 'wizard_id', 'attachment_id',
-                                   string="Shop Photos", required=True,
+                                   string="Shop Photos", required=False,
                                    help="Short Video / Walkway from outdoor / indoor. Must include - signage Board with GST Number.")
 
     shop_videos = fields.Many2many('ir.attachment', 'vendor_kyc_shop_videos_rel', 'wizard_id', 'attachment_id',
-                                   string="Shop Videos", required=True,
+                                   string="Shop Videos", required=False,
                                    help="Short Video / Walkway from outdoor / indoor. Must include - signage Board with GST Number.")
 
-    bank_name = fields.Char(string="Bank Name", required=True)
-    account_no = fields.Char(string="Account Number", required=True)
-    ifsc_code = fields.Char(string="IFSC Code", required=True)
-    bank_address = fields.Char(string="Bank Address", required=True)
+    bank_name = fields.Char(string="Bank Name", required=False)
+    account_no = fields.Char(string="Account Number", required=False)
+    ifsc_code = fields.Char(string="IFSC Code", required=False)
+    bank_address = fields.Char(string="Bank Address", required=False)
     bank_cheque_attachments = fields.Many2many('ir.attachment', 'vendor_kyc_bank_cheque_rel', 'wizard_id',
-                                               'attachment_id', string="Cancelled Cheques", required=True)
+                                               'attachment_id', string="Cancelled Cheques", required=False)
 
     @api.model
     def default_get(self, fields_list):
@@ -121,9 +94,7 @@ class VendorKycWizard(models.TransientModel):
                 # values['street'] = previous_record.street
                 # values['street2'] = previous_record.street2
                 # values['city'] = previous_record.city
-                # values['state_id'] = previous_record.state_id.id
                 # values['pincode'] = previous_record.zip
-                # values['country_id'] = previous_record.country_id.id
                 # values['mobile'] = previous_record.mobile
 
             return values
