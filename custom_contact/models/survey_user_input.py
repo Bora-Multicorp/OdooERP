7103cdd3f9e0373d8631b90b6d5aa60eb2cef26d
odoo <you@example.com>
2025-07-04 11:51:51 +0530

    [ADD] custom_contact : make changes in button string changes

--- custom_contact/models/survey_user_input.py
+++ custom_contact/models/survey_user_input.py
@@ -4,7 +4,6 @@ from odoo import models, _
 from odoo.exceptions import UserError
 import base64
 
-
 class SurveyUserInput(models.Model):
     _inherit = "survey.user_input"
 
@@ -16,7 +15,7 @@ class SurveyUserInput(models.Model):
         super()._mark_done()  # Ensure the base behavior is triggered
 
         vendor_survey = self.env.ref(
-            "survey_upload_file.vendor_kyc_form_survey", raise_if_not_found=False
+            "custom_contact.vendor_kyc_form_survey", raise_if_not_found=False
         )
         if not vendor_survey or self.survey_id != vendor_survey:
             return  # We only process the vendor KYC form
@@ -25,41 +24,40 @@ class SurveyUserInput(models.Model):
         # Map question IDs to partner field names
         question_map = {
             # Basic text/char fields
-            Q("survey_upload_file.email_kyc_survey").id: "email",
-            Q("survey_upload_file.point_of_contact_kyc_survey").id: "point_of_contact",
-            Q("survey_upload_file.business_name_kyc_survey").id: "business_legal_name",
-            Q("survey_upload_file.business_trade_name_kyc_survey").id: "business_trade_name",
-            Q("survey_upload_file.business_address_kyc_survey").id: "business_street",
-            Q("survey_upload_file.business_city_kyc_survey").id: "business_city",
-            Q("survey_upload_file.business_pincode_kyc_survey").id: "business_pincode",
-            Q("survey_upload_file.additional_address_kyc_survey").id: "additional_street",
-            Q("survey_upload_file.additional_city_kyc_survey").id: "additional_city",
-            Q("survey_upload_file.additional_pincode_kyc_survey").id: "additional_zip",
-            Q("survey_upload_file.additional_contact_no_kyc_survey").id: "additional_phone",
-            Q("survey_upload_file.additional_email_kyc_survey").id: "additional_email",
-            #Q("survey_upload_file.business_constitution_kyc_survey").id: "const_business",
-            Q("survey_upload_file.director_name_kyc_survey").id: "director_name",
-            Q("survey_upload_file.director_contact_no_kyc_survey").id: "director_phone",
-            Q("survey_upload_file.director_email_kyc_survey").id: "director_email",
-            Q("survey_upload_file.gst_no_kyc_survey").id: "gst_no",
-            Q("survey_upload_file.udyam_certificate_kyc_survey").id: "udyam_number",
-            Q("survey_upload_file.gst_duration_kyc_survey").id: "gst_return_duration",
-            Q("survey_upload_file.bank_name_kyc_survey").id: "bank_name",
-            Q("survey_upload_file.account_no_kyc_survey").id: "account_no",
-            Q("survey_upload_file.ifsc_code_kyc_survey").id: "ifsc_code",
-            Q("survey_upload_file.bank_address_kyc_survey").id: "bank_address",
+            Q("custom_contact.email_kyc_survey").id: "email",
+            Q("custom_contact.point_of_contact_kyc_survey").id: "point_of_contact",
+            Q("custom_contact.business_name_kyc_survey").id: "business_legal_name",
+            Q("custom_contact.business_trade_name_kyc_survey").id: "business_trade_name",
+            Q("custom_contact.business_address_kyc_survey").id: "business_street",
+            Q("custom_contact.business_city_kyc_survey").id: "business_city",
+            Q("custom_contact.business_pincode_kyc_survey").id: "business_pincode",
+            Q("custom_contact.additional_address_kyc_survey").id: "additional_street",
+            Q("custom_contact.additional_city_kyc_survey").id: "additional_city",
+            Q("custom_contact.additional_pincode_kyc_survey").id: "additional_zip",
+            Q("custom_contact.additional_contact_no_kyc_survey").id: "additional_phone",
+            Q("custom_contact.additional_email_kyc_survey").id: "additional_email",
+            #Q("custom_contact.business_constitution_kyc_survey").id: "const_business",
+            Q("custom_contact.director_name_kyc_survey").id: "director_name",
+            Q("custom_contact.director_contact_no_kyc_survey").id: "director_phone",
+            Q("custom_contact.director_email_kyc_survey").id: "director_email",
+            Q("custom_contact.gst_no_kyc_survey").id: "gst_no",
+            Q("custom_contact.udyam_certificate_kyc_survey").id: "udyam_number",
+            Q("custom_contact.gst_duration_kyc_survey").id: "gst_return_duration",
+            Q("custom_contact.bank_name_kyc_survey").id: "bank_name",
+            Q("custom_contact.account_no_kyc_survey").id: "account_no",
+            Q("custom_contact.ifsc_code_kyc_survey").id: "ifsc_code",
+            Q("custom_contact.bank_address_kyc_survey").id: "bank_address",
 
             # File uploads
-            Q("survey_upload_file.aadhaar_card_kyc_survey").id: "aadhaar_card",
-            Q("survey_upload_file.pan_card_kyc_survey").id: "pan_card",
-            Q("survey_upload_file.gst_certificate_kyc_survey").id: "gst_certificate",
-            Q("survey_upload_file.udyam_document_kyc_survey").id: "udyam_document",
-            Q("survey_upload_file.shop_photos_kyc_survey").id: "shop_photos",
-            Q("survey_upload_file.shop_videos_kyc_survey").id: "shop_videos",
-            Q("survey_upload_file.cancel_cheque_kyc_survey").id: "bank_cheque_attachments",
+            Q("custom_contact.aadhaar_card_kyc_survey").id: "aadhaar_card",
+            Q("custom_contact.pan_card_kyc_survey").id: "pan_card",
+            Q("custom_contact.gst_certificate_kyc_survey").id: "gst_certificate",
+            Q("custom_contact.udyam_document_kyc_survey").id: "udyam_document",
+            Q("custom_contact.shop_photos_kyc_survey").id: "shop_photos",
+            Q("custom_contact.shop_videos_kyc_survey").id: "shop_videos",
+            Q("custom_contact.cancel_cheque_kyc_survey").id: "bank_cheque_attachments",
         }
         values = {}
-
         for line in self.user_input_line_ids:
             field_name = question_map.get(line.question_id.id)
             if not field_name:
@@ -100,13 +98,16 @@ class SurveyUserInput(models.Model):
                     # Multiple file upload (Many2many)
                     values[field_name] = [(6, 0, files.ids)]
         if values:
+            print('111111111')
             if self.partner_id:
+                print('22222222222')
                 #self.partner_id.write(values)
                 values['partner_id'] = self.partner_id.id
                 res = self.env['res.partner.kyc.approval'].create(values)
                 if res:
                     self.partner_id.write({'is_kyc': True})
             elif self.email:
+                print('3333333333')
                 partner = self.env['res.partner'].search([('email', '=', self.email)], limit=1)
 
                 #partner.write(values)
