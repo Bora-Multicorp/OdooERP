b0c9d903dac27e2be52a66e9bbed8a4e06cb944d
chetna.modi <chetna.modi@sunarctechnologies.com>
2025-07-18 11:33:55 +0530

    add custom product packaging

---
+++ custom_product/__manifest__.py
@@ -0,0 +1,16 @@
+
+{
+    "name": "Custom Product Packaging",
+    "version": "0.1",
+    "category": "Uncategorized",
+    "author": "",
+    "website": "",
+    "license": "AGPL-3",
+    'depends': ['stock', 'product','web'],
+    "data": [
+             "security/ir.model.access.csv",
+             # "views/packaging.xml",
+             "views/product_packaging.xml"],
+
+    "installable": True,
+}
