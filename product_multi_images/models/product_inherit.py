32c7954dc821abb1715db912436bccff00c57806
Anand <anand@Anands-MacBook-Air.local>
2025-07-19 21:27:12 +0530

    image_5 added

--- product_multi_images/models/product_inherit.py
+++ product_multi_images/models/product_inherit.py
@@ -17,3 +17,4 @@ class ProductTemplate(models.Model):
     image_2 = fields.Image("Image 2")
     image_3 = fields.Image("Image 3")
     image_4 = fields.Image("Image 4")
+    image_5 = fields.Image("Image 5")
