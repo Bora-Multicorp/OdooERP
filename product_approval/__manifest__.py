1037258195a64d60eaf3c4815bfe1a396859b54c
Anand <anand@Anands-MacBook-Air.local>
2025-07-29 10:37:11 +0530

    issue fixing

--- product_approval/__manifest__.py
+++ product_approval/__manifest__.py
@@ -20,7 +20,7 @@
     'assets': {
         'web.assets_backend': [
             'product_approval/static/src/xml/ribbon.xml',
-            'product_approval/static/src/js/make_form_readonly.js'
+            # 'product_approval/static/src/js/make_form_readonly.js'
         ],
     },
     'installable': True,
