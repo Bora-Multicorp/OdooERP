42c12334767a2671373fb3568e77324e4e05b223
chetna.modi <chetna.modi@sunarctechnologies.com>
2025-08-01 15:55:06 +0530

    add task stock valution

---
+++ bora_dubai/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Stock Valuation in USD",
+    "version": "1.0",
+    "depends": ["stock", "account", "stock_account"],
+    "author": "Custom",
+    "category": "Inventory",
+    "description": "Show stock valuation in USD based on exchange rate.",
+    "data": [
+        "views/stock_valuation.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
+
