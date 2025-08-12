4f3d5db20c0d747db898a131ca4490b535f957e8
prakash.jain <prakash.jain@sunarctechnologies.com>
2025-08-12 14:37:04 +0530

    Attachment file limit and total warning msg updated

--- sh_survey_extra_fields/static/src/js/sh_survey_extra_fields.js
+++ sh_survey_extra_fields/static/src/js/sh_survey_extra_fields.js
@@ -83,7 +83,8 @@ SurveyFormWidget.include({
 
     const maxFilesPerField = {
         'shop photos': 10,
-        'shop videos': 10
+        'shop videos': 10,
+        'electricity bill': 5
     };
 
     const sizeLimits = {
@@ -108,6 +109,7 @@ SurveyFormWidget.include({
     const isShopPhotos = sizeKey.includes("shop photos");
     const isAadhaar = sizeKey.includes("aadhaar card");
     const isPan = sizeKey.includes("pan card");
+    const isElectricityBill = sizeKey.includes("electricity bill");
 
     let totalSize = 0;
     const dictKey = $fileUpload[0].name;
@@ -128,12 +130,22 @@ SurveyFormWidget.include({
             $fileUpload.val('');
             return;
         }
-        if (isShopPhotos || isShopVideos) {
+        if (isShopPhotos || isShopVideos || isElectricityBill) {
             let existingCount = self.SH_FILE_DATA_DICTIONARY[dictKey] ? self.SH_FILE_DATA_DICTIONARY[dictKey].length : 0;
             let maxAllowed = maxFilesPerField[sizeKey];
             if (existingCount + $fileUpload[0].files.length > maxAllowed) {
                 alert(`"${sizeKey}" allows only ${maxAllowed} file${maxAllowed > 1 ? 's' : ''}.`);
                 $fileUpload.val('');
+                return;
+            }
+            totalSize += file.size;
+            if (totalSize > maxSizePerField) {
+                alert(
+                    `Total upload for "${sizeKey}" exceeds ${ (maxSizePerField / 1024 / 1024).toFixed(1) } MB. `
+                    + `You selected ${ (totalSize / 1024 / 1024).toFixed(1) } MB.`
+                );
+                $fileUpload.val('');
+                delete self.SH_FILE_DATA_DICTIONARY[dictKey];
                 return;
             }
         }
@@ -148,14 +160,9 @@ SurveyFormWidget.include({
             $fileUpload.val('');
             return;
         }
-        totalSize += file.size;
-        if (totalSize > maxSizePerField) {
-            alert(
-                `Total upload for "${sizeKey}" exceeds ${ (maxSizePerField / 1024 / 1024).toFixed(1) } MB. `
-                + `You selected ${ (totalSize / 1024 / 1024).toFixed(1) } MB.`
-            );
+         if (file.size > maxSizePerField) {
+            alert(`Each file for "${sizeKey}" must be ≤ ${(maxSizePerField / 1024 / 1024).toFixed(1)} MB.`);
             $fileUpload.val('');
-            delete self.SH_FILE_DATA_DICTIONARY[dictKey];
             return;
         }
         const result = await toBase64(file);
