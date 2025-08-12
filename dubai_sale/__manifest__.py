7393eac5a2e0b799ec26d5e4312c94298ee5bfe3
Anand <anand@Anands-MacBook-Air.local>
2025-08-12 11:40:45 +0530

    translations

--- dubai_sale/__manifest__.py
+++ dubai_sale/__manifest__.py
@@ -31,7 +31,8 @@ Long description of module's purpose
         'views/unlock_button_action_inherited.xml',
         'views/account_move_inherited.xml',
         'views/add_documents.xml',
-        'views/pod_in_stock_picking.xml',        
+        'views/pod_in_stock_picking.xml',   
+        'views/quotation_to_pi.xml',     
         'data/account_group_in_sales.xml'
     ],
     # only loaded in demonstration mode
