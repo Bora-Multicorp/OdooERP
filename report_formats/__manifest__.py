dc13981000dda58d0ec61f1129fbf5c74f3e14e4
chetna.modi <chetna.modi@sunarctechnologies.com>
2025-08-13 13:08:09 +0530

    add quotation format and invoice format

---
+++ report_formats/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Bora dubai report formats",
+    "version": "1.0",
+    "depends": ["sale","account"],
+    "author": "Custom",
+    "category": "Sale",
+    "description": "Reports format",
+    "data": [
+        "data/voucher_buyer_no.xml",
+        "views/account_move.xml",
+        "views/sale_order.xml",
+        "report/sale_report.xml",
+        "report/sale_invoice.xml"
+    ],
+
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
+
---
+++ report_formats/models/account_move.py
@@ -0,0 +1,12 @@
+# models/account_move.py
+from odoo import models, fields
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+
+    dispatch_id = fields.Char(string="Despatch Through")
+    delivery_note_date = fields.Date(string="Delivery Note Date")
+    delivery_note = fields.Char(string="Delivery Note")
+    despatch_doc_no = fields.Char(string="Dispatch Document no.")
+    buyer_date = fields.Date(string="Buyer Date")
