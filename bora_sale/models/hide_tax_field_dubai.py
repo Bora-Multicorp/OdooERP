2e6efe4c10ebe017ccf3b0eb1dcd591e41176814
Anand Suthar <anand.suthar@sunarctechnologies..com>
2025-09-03 16:24:50 +0530

    fix readonly field issue

--- bora_sale/models/hide_tax_field_dubai.py
+++ bora_sale/models/hide_tax_field_dubai.py
@@ -42,6 +42,30 @@ class HideTaxFieldFromPurchaseOrderForm(models.Model):
                 continue
 
 
+# make tax field readonly for sale order line
+class ReadonlyTaxFieldFromSaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    read_only_tax_field = fields.Boolean(
+        compute='_compute_readonly_tax_column_if_both_dubai'
+    )
+
+
+    @api.depends('product_id')
+    def _compute_readonly_tax_column_if_both_dubai(self):
+
+        # 1. check if product type is goods
+        self.read_only_tax_field = False
+        is_product_type_goods = False
+        for line in self:
+            if line.product_id:
+                if line.product_id.product_tmpl_id.type == 'consu':
+                    is_product_type_goods = True
+            company_registry = line.order_id.company_id.company_registry
+            if (company_registry == '1804237.01' or company_registry == '3892') and is_product_type_goods:
+                line.read_only_tax_field = True
+
+
 
 # hide tax fields from sale order form
 class HideTaxFieldFormSaleOrder(models.Model):
@@ -51,10 +75,8 @@ class HideTaxFieldFormSaleOrder(models.Model):
         compute='_compute_hide_tax_column_if_both_dubai'
     )
 
-
     @api.depends('partner_id.country_id.code','order_line.product_id.product_tmpl_id')
     def _compute_hide_tax_column_if_both_dubai(self):
-
 
         for order in self:
             order.hide_tax_column = False
