210b03e02be71b33829cee74ac871c559b76d850
Anand Suthar <anand.suthar@sunarctechnologies..com>
2025-09-04 16:27:37 +0530

    comment out QC flow

--- bora_purchase/__manifest__.py
+++ bora_purchase/__manifest__.py
@@ -29,7 +29,7 @@ Bora purchase operations
         'wizard/po_confirm_approval/po_confirm_reject_request_wizard.xml',
         'wizard/po_cancel_approval/po_cancel_approve_request_wizard.xml',
         'wizard/po_cancel_approval/po_cancel_reject_request_wizard.xml',
-        'views/qc_check_on_delivery.xml',
+        # 'views/qc_check_on_delivery.xml',
         'views/vendor_payment_email_template.xml',
         'views/po_unlock_approvers.xml',
         'views/po_unlock.xml',
--- bora_purchase/models/__init__.py
+++ bora_purchase/models/__init__.py
@@ -3,7 +3,7 @@
 
 from . import po_approvers
 from . import po_unlock
-from . import qc_check_on_delivery
+# from . import qc_check_on_delivery
 from . import send_email_to_vendor_on_payment
 from . import bill_confirm
 from . import po_fullfill_by
