31a3476f44c85d2d7f60f890038a00569b20e3c4
Anand Suthar <anand.suthar@sunarctechnologies..com>
2025-09-08 12:50:48 +0530

    multi copmany issue fix

--- contact_multi_company/models/res_partner.py
+++ contact_multi_company/models/res_partner.py
@@ -4,6 +4,23 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import api, fields, models
+from odoo.exceptions import AccessError
+
+
+# class ResPartner(models.Model):
+#     _inherit = "res.partner"
+
+#     company_ids = fields.Many2many(
+#         comodel_name="res.company",
+#         column1="partner_id",
+#         column2="company_id",
+#         relation="res_partner_company_rel",
+#     )
+
+#     @api.model
+#     def search(self, args, offset=0, limit=None, order=None):
+#         dom = self.env["multi.company.abstract"]._patch_company_domain(args)
+#         return super().search(dom, offset=offset, limit=limit, order=order)
 
 
 class ResPartner(models.Model):
