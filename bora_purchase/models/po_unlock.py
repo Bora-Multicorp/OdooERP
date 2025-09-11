85f24ea9804f65a0d291004153b517329a43b1f1
Anand Suthar <anand.suthar@sunarctechnologies..com>
2025-09-11 16:14:22 +0530

    issue fix

--- bora_purchase/models/po_unlock.py
+++ bora_purchase/models/po_unlock.py
@@ -65,6 +65,12 @@ class PurchaseOrderUnlock(models.Model):
 
     def suspend_unlock_process(self, remark):
 
+        self.message_post(
+            body=f"Approval suspended by {self.env.user.display_name}. Reason: {remark}",
+            message_type="comment",
+            subtype_xmlid="mail.mt_note"
+        )
+
         for order in self:
 
             pending_approvers = order.approval_users_ids.filtered(lambda u: not u.state)
