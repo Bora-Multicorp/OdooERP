b470f00cad758698c86d328b1978f6e886f41922
chetna.modi <chetna.modi@sunarctechnologies.com>
2025-09-16 14:45:29 +0530

    change in confirmation approval flow acc. to client

--- bora_sale_purchase_approval/models/sale_cancel_approval_res_config.py
+++ bora_sale_purchase_approval/models/sale_cancel_approval_res_config.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
+from odoo import fields, models, api
 import random
 
-class SaleOrderApprovalConfig(models.Model):
-    _name = "sale.order.approval.config"
-    _description = "Sales Order Approval Settings"
-    _rec_name = 'user_id'
 
-    sequence = fields.Integer(string='Sequence', index=True)
+class SOCancellationApproverTeam(models.Model):
+    _name = 'sale.order.cancellation.approvers'
+    _description = 'Cancelation Approvers'
+    _rec_name = 'user_id'
+    _order = 'sequence'
+
+    sequence = fields.Integer(string='Sequence', readonly=True)
     user_id = fields.Many2one('res.users', string='User', required=True)
     default_approver = fields.Selection([
         ('approver1', 'Approver 1'),
@@ -24,7 +24,7 @@ class SaleOrderApprovalConfig(models.Model):
     @api.depends('user_id')
     def _compute_existing_user_ids(self):
         # Get all existing users' IDs
-        existing_user_ids_list = self.env['sale.order.approval.config'].search([]).user_id.ids
+        existing_user_ids_list = self.env['sale.order.cancellation.approvers'].search([]).user_id.ids
 
         # Convert the list to a set for efficient removal
         existing_user_ids_set = set(existing_user_ids_list)
@@ -38,25 +38,47 @@ class SaleOrderApprovalConfig(models.Model):
             # Assign the remaining IDs back to the field
             record.existing_user_ids = [(6, 0, list(existing_user_ids_set))]
 
+    # generate sequence
     @api.model
     def default_get(self, fields_list):
         defaults = super().default_get(fields_list)
-        if 'sequence' in fields_list:
-            max_sequence = self.search([], order="sequence desc", limit=1).sequence or 0
-            defaults['sequence'] = max_sequence + 1
+        group_id = defaults.get('group')
+        if group_id:
+            max_sequence = self.search([('group', '=', group_id)], order="sequence desc", limit=1).sequence
+            defaults['sequence'] = (max_sequence or 0) + 1
+        else:
+            # If no group is present (e.g., from a form view), find the highest global sequence.
+            max_sequence = self.search([], order="sequence desc", limit=1).sequence
+            defaults['sequence'] = (max_sequence or 0) + 1
+
         return defaults
+
+    @api.constrains('default_user', 'group')
+    def _check_unique_default_user_per_group(self):
+        for record in self:
+            if record.default_user:
+                existing_default_users = self.search([
+                    ('id', '!=', record.id),
+                    ('group', '=', record.group),
+                    ('default_user', '=', True)
+                ])
+                if existing_default_users:
+                    # Reset the previous default user to False
+                    existing_default_users.write({'default_user': False})
+
+    @api.onchange('default_user', 'group')
+    def _onchange_default_user(self):
+        if self.default_user:
+            existing_default_users = self.search([
+                ('id', '!=', self._origin.id if self._origin else False),
+                ('group', '=', self.group),
+                ('default_user', '=', True)
+            ])
+            if existing_default_users:
+                pass
 
     @api.model
     def create(self, vals):
-
-        # 1. to make sure there will be only one approver and only one approver2
-        if vals.get('default_approver'):
-            # Find the old approver of the same type and reset them
-            self.search([
-                ('default_approver', '=', vals['default_approver'])
-            ]).write({'default_approver': False})
-
-        # 2. Assign color code
         if not vals.get('color'):
             # Get all used colors
             used_colors = set(self.search([]).mapped('color'))
@@ -73,31 +95,19 @@ class SaleOrderApprovalConfig(models.Model):
 
         return super().create(vals)
 
-    def write(self, vals):
-        # To make sure there will be only one approver and only one approver2
-        if vals.get('default_approver'):
-            # Find the old approver of the same type and reset them
-            self.search([
-                ('id', '!=', self.id),
-                ('default_approver', '=', vals['default_approver'])
-            ]).write({'default_approver': False})
-
-        return super().write(vals)
-
     def unlink(self):
-        # Step 1: Collect all affected purchase orders
         affected_pos = self.env['sale.order']  # empty recordset
 
         for approver in self:
             # Find all POs where this approver is linked
             pos = self.env['sale.order'].search([
-                ('so_approval_users_ids_for_confirmation.user_id', '=', approver.user_id.id)
+                ('so_approval_users_ids_for_cancellation.user_id', '=', approver.user_id.id)
             ])
             affected_pos |= pos  # accumulate affected POs
 
             # Delete linked PO approval users with empty state
             for so in pos:
-                records_to_delete = so.approval_users_ids_for_confirmation.filtered(
+                records_to_delete = so.so_approval_users_ids_for_cancellation.filtered(
                     lambda r: r.user_id == approver.user_id and not r.state
                 )
                 records_to_delete.unlink()
@@ -106,13 +116,12 @@ class SaleOrderApprovalConfig(models.Model):
         result = super().unlink()
 
         # Step 3: Re-sequence all remaining approvers globally
-        all_approvers = self.env['sale.order.approval.config'].search([], order='sequence')
+        all_approvers = self.env['sale.order.cancellation.approvers'].search([], order='sequence')
         for idx, record in enumerate(all_approvers, start=1):
             record.sequence = idx
 
-        # Step 4: Re-sequence PO approval users per purchase order
         for so in affected_pos:
-            so_users = so.so_approval_users_ids_for_confirmation.sorted(key='sequence')
+            so_users =so.so_approval_users_ids_for_cancellation.sorted(key='sequence')
             for idx, user in enumerate(so_users, start=1):
                 user.sequence = idx
 
--- bora_sale_purchase_approval/models/sale_order_cancel_approval.py
+++ bora_sale_purchase_approval/models/sale_order_cancel_approval.py
@@ -9,169 +9,254 @@ _logger = logging.getLogger(__name__)
 class SaleOrderCancellationApproval(models.Model):
     _inherit = 'sale.order'
 
-    # Cancellation approval fields
-    cancel_is_hidden_for_approval = fields.Boolean(default=False)
-    cancel_is_approved = fields.Boolean(string='Cancel Approved', default=False,
-                                 help="Indicates if the sales order cancellation has been approved.")
+    old_state = fields.Char()
+    so_assigned_to_form_cancellation = fields.Many2one('res.users', string='Assigned To', tracking=True)
+    so_approval_users_ids_for_cancellation = fields.One2many('so.cancellation.approval.users',
+                                                          'so_cancellation_approval_id',
+                                                          'Cancellation SO Approval Authorities',
+                                                          help='SO cancellation approval authority details')
 
-    existing_user_ids = fields.Many2many('res.users', compute='_compute_cancel_existing_users', store=True)
+    def assign_cancel_users(self, so_cancellation_approval_users):
 
-    cancel_state = fields.Selection([
-        ('cancel_draft', 'Cancel Draft'),
-        ('cancel_pending', 'Waiting for Cancel Approval'),
-        ('cancel_approved', 'Cancel Approved'),
-        ('cancel_rejected', 'Cancel Rejected'),
-    ], string="Cancel State", default='cancel_draft')
+        so_cancel_approval_user_vals = []
+        for approval in so_cancellation_approval_users:
+            so_cancel_approval_user_vals.append((0, 0, {
+                'sequence': approval.sequence,
+                'user_id': approval.user_id.id,
+            }))
+        self.write({
+            'so_approval_users_ids_for_cancellation': so_cancel_approval_user_vals,
+            'old_state': self.state,
+            'state': 'cancellation_pending'
+        })
 
-    cancel_approval_users_ids = fields.One2many(
-        'sale.order.cancel.approval.users', 'sale_order_id', 'Cancel Approval Authorities',
-        help='Approval Authority Details for Cancellation')
-    cancel_assigned_to = fields.Many2one('res.users', string='Cancel Assigned To')
-
-    # ========== DEFAULT APPROVERS ==========
-    # @api.model
-    # def default_get(self, fields_list):
-    #     defaults = super().default_get(fields_list)
-    #     cancel_config_users = self.env['sale.order.approval.config'].sudo().search([])
-    #     if cancel_config_users:
-    #         approval_user_vals = []
-    #         for approval in cancel_config_users:
-    #             approval_user_vals.append((0, 0, {
-    #                 'sequence': approval.sequence,
-    #                 'user_id': approval.user_id.id,
-    #             }))
-    #         defaults['cancel_approval_users_ids'] = approval_user_vals
-    #     return defaults
-
-    # ========== ACTIONS ==========
-    def action_cancel_request(self):
-        """
-        Custom action triggered when user clicks Cancel.
-        Starts the cancel approval process.
-        """
-        self.ensure_one()
-
-        if self.state == 'cancel_draft':
-            # If no cancel approval users already, add defaults
-            if not self.cancel_approval_users_ids:
-                cancel_config_users = self.env['sale.order.approval.config'].sudo().search([])
-                if not cancel_config_users:
+        if self.so_assigned_to_form_cancellation:
+            for user_id in self.so_approval_users_ids_for_cancellation:
+                if user_id.state == 'reject':
                     raise ValidationError(
-                        _("Please configure Cancel Approval Authority before submitting the request."))
+                        f"Cancellation request is rejected by '{user_id.user_id.name}', please review 'Cancellation Approval Authorities' tab for more details.")
+            raise ValidationError(
+                f"Cancellation request is now pending from '{self.so_assigned_to_form_confirmation.name}'.")
 
-                approval_user_vals = []
-                for approval in cancel_config_users:
-                    approval_user_vals.append((0, 0, {
-                        'sequence': approval.sequence,
-                        'user_id': approval.user_id.id,
-                    }))
+        if self.id:
+            self._update_assigned_to_form_SO_cancellation()
 
-                self.write({'cancel_approval_users_ids': approval_user_vals})
+        self._create_activity_and_send_notification_for_cancel_request()
 
-            # Update state and assigned_to
-            self.write({
-                'cancel_state': 'cancel_pending',
-                'cancel_is_hidden_for_approval': True
-            })
-            self._update_cancel_assigned_to()
-            return
+    def action_cancel(self):
+        so_cancellation_approval_users = self.env['sale.order.cancellation.approvers'].sudo().search([])
+        if not so_cancellation_approval_users:
+            raise ValidationError("Please add cancellation authority before submit request.")
 
-        # If not in custom_draft → run default cancel flow
-        return super(SaleOrderCancellationApproval, self).action_cancel()
+        return self.env.ref(
+            "bora_sale_purchase_approval.action_cancelation_approval_user_picker_wizard"
+        ).sudo().read()[0]
 
-
-    @api.depends('cancel_approval_users_ids.user_id')
-    def _compute_cancel_existing_users(self):
-        for record in self:
-            if record.cancel_approval_users_ids:
-                record.existing_user_ids = [(6, 0, record.cancel_approval_users_ids.mapped('user_id').ids)]
-            else:
-                record.existing_user_ids = [(6, 0, [])]
-
-    def _update_cancel_assigned_to(self, send_notification=True):
+    def _update_assigned_to_form_SO_cancellation(self):
         for rec in self:
             next_user = None
-            for line in sorted(rec.cancel_approval_users_ids, key=lambda x: x.sequence):
+            last_user_approval_state = ''
+            for line in sorted(rec.so_approval_users_ids_for_cancellation, key=lambda x: x.sequence):
+                last_user_approval_state = line.state
                 if not line.state:
                     next_user = line.user_id
                     break
-            rec.cancel_assigned_to = next_user
+            rec.so_assigned_to_form_cancellation = next_user
 
-            if rec.cancel_assigned_to:
-                rec._create_cancel_sale_order_activity_and_send_notification(send_notification)
+            if not rec.so_assigned_to_form_cancellation:
+                if last_user_approval_state == 'suspended' or last_user_approval_state == 'reject':
+                    self.write({
+                        'state': self.old_state
+                    })
+                else:
+                    super(SaleOrderCancellationApproval, self).action_cancel()
 
-    def _update_cancel_state_based_on_approvals(self):
-        for rec in self:
-            states = rec.cancel_approval_users_ids.mapped('state')
-            if any(s == 'reject' for s in states):
-                rec.cancel_state = 'cancel_rejected'
-            elif states and all(s == 'approve' for s in states):
-                rec.cancel_state = 'cancel_approved'
-                rec.cancel_is_hidden_for_approval = False
-                # now really cancel the order
-                rec.action_cancel()
+    def _send_notification_on_rejection_of_SO_cancellation(self):
 
-    def _create_cancel_sale_order_activity_and_send_notification(self, send_notification=True):
-        for rec in self:
-            rec.activity_schedule(
-                act_type_xmlid='mail.mail_activity_data_todo',
-                summary=f'Sales Order Cancellation approval for: {rec.name}',
-                note=_("You have been assigned to review this sales order cancellation."),
-                user_id=rec.cancel_assigned_to.id,
-                date_deadline=fields.Date.context_today(self),
-            )
-            if send_notification:
-                # Notify approver
-                rec.env['bus.bus']._sendone(
-                    rec.cancel_assigned_to.partner_id,
+        approval_users = self.env['sale.order.cancellation.approvers'].sudo().search([])
+
+        self.write({
+            'so_assigned_to_form_confirmation': None
+        })
+
+        for user in approval_users:
+            if user.user_id == self.env.user:
+                self.env['bus.bus']._sendone(
+                    user.user_id.partner_id,
                     'simple_notification',
                     {
-                        'type': 'warning',
-                        'title': _("Sales Order Cancellation approval for %s") % rec.name,
-                        'message': _("Activity assigned to you."),
+                        'type': 'info',
+                        'title': f'SO cancel approval request for {self.name}, successfully rejected by you.',
+                        'message': f'',
+                        'sticky': True,
+                    },
+                )
+            else:
+                self.env['bus.bus']._sendone(
+                    user.user_id.partner_id,
+                    'simple_notification',
+                    {
+                        'type': 'danger',
+                        'title': f'SO cancel approval request for {self.name}, rejected by {self.env.user.name}.',
+                        'message': f'',
                         'sticky': True,
                     },
                 )
 
-            # Notify requester
-            if self.env.user.partner_id != rec.cancel_assigned_to.partner_id:
+    def _create_activity_and_send_notification_on_cancellation_approval(self):
+        print("ddddddddddddddddd2222222222222222222222")
+
+        for rec in self:
+
+            if not rec.so_assigned_to_form_cancellation:
+                rec.env['bus.bus']._sendone(
+                    rec.create_uid.partner_id,
+                    'simple_notification',
+                    {
+                        'type': 'success',
+                        'title': f'SO {rec.name} successfully cancelled by approver, you can now proceed further.',
+                        'message': '',
+                        'sticky': True,
+                    },
+                )
+
+            else:
+
+                # 1. schedule activity for next assignee
+                rec.activity_schedule(
+                    act_type_xmlid='mail.mail_activity_data_todo',
+                    summary=f'SO {rec.name} cancel approval request assigned to you.',
+                    note="You have been assigned to cancel this SO.",
+                    user_id=rec.so_assigned_to_form_cancellation.id,
+                    date_deadline=fields.Date.context_today(self),
+                )
+
+                # 2. send notification to next approver
+                rec.env['bus.bus']._sendone(
+                    rec.so_assigned_to_form_cancellation.partner_id,
+                    'simple_notification',
+                    {
+                        'type': 'success',
+                        'title': f'SO cancellation approval request for {rec.name}, assigned to you.',
+                        'message': 'You have been assigned to cancel this SO.',
+                        'sticky': True,
+                    },
+                )
+
+            # 4. in the last send info message to self
+            rec.env['bus.bus']._sendone(
+                self.env.user.partner_id,
+                'simple_notification',
+                {
+                    'type': 'info',
+                    'title': f'SO cancellation approval request successfully submitted by you.',
+                    'message': '',
+                    'sticky': True,
+                },
+            )
+
+    def _create_activity_and_send_notification_for_cancel_request(self):
+        for rec in self:
+            rec.activity_schedule(
+                act_type_xmlid='mail.mail_activity_data_todo',
+                summary=f'SO cancel approval for order: {rec.name}',
+                note="You have been assigned to cancel this SO.",
+                user_id=rec.so_assigned_to_form_cancellation.id,
+                date_deadline=fields.Date.context_today(self),
+            )
+
+            rec.env['bus.bus']._sendone(
+                rec.so_assigned_to_form_cancellation.partner_id,
+                'simple_notification',
+                {
+                    'type': 'success',
+                    'title': f'SO cancel approval request for {rec.name}, assigned to you.',
+                    'message': f'Activity assigned to you.',
+                    'sticky': True,
+                },
+            )
+
+            if rec.so_assigned_to_form_cancellation.partner_id != self.env.user.partner_id:
                 rec.env['bus.bus']._sendone(
                     self.env.user.partner_id,
                     'simple_notification',
                     {
                         'type': 'info',
-                        'title': f"Sale Order '{rec.name}' successfully submitted for cancellation approval",
-                        'message': "",
+                        'title': f'SO {rec.name} cancel approval request sent successfully.',
+                        'message': '',
                         'sticky': True,
                     },
                 )
 
+    def action_so_suspend(self):
+        return self.env.ref(
+            "bora_sale_purchase_approval.cnacel_suspend_so_confirm_wizard_action"
+        ).sudo().read()[0]
 
-# ========== APPROVAL USERS FOR CANCELLATION ==========
-class SaleOrderCancelApprovalUsers(models.Model):
-    _name = "sale.order.cancel.approval.users"
-    _rec_name = 'user_id'
-    _description = "Sales Order Cancel Approval Users"
-    _order = "sequence"
+    def suspend_so_cancelation_process(self, remark):
+
+        self.message_post(
+            body=f"Approval suspended by {self.env.user.display_name}. Reason: {remark}",
+            message_type="comment",
+            subtype_xmlid="mail.mt_note"
+        )
+        print("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm")
+
+        for order in self:
+            pending_approvers = order.so_approval_users_ids_for_cancellation.filtered(lambda u: not u.state)
+
+            pending_approvers.write({
+                'state': 'suspended',
+                'remark': f"By {self.env.user.name} - " + (f" {remark}" if remark else ""),
+                'action_date': fields.Datetime.now()})
+
+            activities = self.env['mail.activity'].search([
+                ('res_model', '=', 'sale.order'),
+                ('res_id', '=', self.ids),
+                ('user_id', 'in', pending_approvers.mapped('user_id').ids),
+                ('activity_type_id', '=', self.env.ref('mail.mail_activity_data_todo').id),
+            ])
+
+            order.write({'so_assigned_to_form_cancellation': None, 'state': self.old_state})
+
+            # send notification to creator
+            order.env['bus.bus']._sendone(
+                order.create_uid.partner_id,
+                'simple_notification',
+                {
+                    'type': 'danger',
+                    'title': f'SO {order.name} cnacellation suspended by {self.env.user.name}. you need to initiate the approval process again.',
+                    'message': '',
+                    'sticky': True,
+                },
+            )
+
+            # 4. in the last send info message to self
+            order.env['bus.bus']._sendone(
+                self.env.user.partner_id,
+                'simple_notification',
+                {
+                    'type': 'info',
+                    'title': f'Cancellation process suspended successfully.',
+                    'message': '',
+                    'sticky': True,
+                },
+            )
+
+            activities.unlink()
+
+
+class SOCancellationApprovalUsers(models.Model):
+    _name = "so.cancellation.approval.users"
+    _rec_name = 'so_cancellation_approval_id'
+    _description = "SO Cancellation Approval Users"
+    _order = "create_date, sequence"
 
     sequence = fields.Integer(string='Sequence')
-    sale_order_id = fields.Many2one('sale.order', string="Sales Order")
+    so_cancellation_approval_id = fields.Many2one('sale.order', string="SO Cancellation Approval")
+    job_id = fields.Char(string="Designation", readonly=True)
     user_id = fields.Many2one('res.users', string='User', required=True)
-    state = fields.Selection([('approve', 'Approved'), ('reject', 'Rejected')], string="Action")
+    state = fields.Selection([('approve', 'Approved'), ('reject', 'Rejected'), ('suspended', 'Suspended')],
+                             string="Action")
     remark = fields.Char('Remarks', tracking=True)
     action_date = fields.Datetime(string="Action Date")
-
-    def write(self, vals):
-        res = super().write(vals)
-        for rec in self:
-            if rec.sale_order_id:
-                rec.sale_order_id._update_cancel_state_based_on_approvals()
-        return res
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        res_list = super(SaleOrderCancelApprovalUsers, self).create(vals_list)
-        for res in res_list:
-            if res.sale_order_id:
-                res.sale_order_id._update_cancel_state_based_on_approvals()
-        return res_list
