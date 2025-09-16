7395ed8d8788d33de5d9cd1978b0a064bd50f564
Anand Suthar <anand.suthar@sunarctechnologies.com>
2025-09-16 16:42:55 +0530

    Update po_confirm.py

--- bora_purchase/models/po_confirm.py
+++ bora_purchase/models/po_confirm.py
@@ -22,7 +22,7 @@ class PurchaseOrderConfirmApproval(models.Model):
 
 
     def assign_users(self, po_confirm_approval_users):
-        super(PurchaseOrderConfirmApproval, self).button_confirm()
+        # super(PurchaseOrderConfirmApproval, self).button_confirm()
 
         po_confirm_approval_user_vals = []
         for approval in po_confirm_approval_users:
