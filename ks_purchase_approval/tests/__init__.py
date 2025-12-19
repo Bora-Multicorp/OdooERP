4d12666a2e4352c2d9b6fc53c2df434513c3ec35
Ayush-Ksolves <ayush.shrivastava@ksolves.com>
2025-12-19 11:55:22 +0530

    Update

--- ks_purchase_approval/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import common
-from . import test_ks_purchase_approval_config
-from . import test_ks_purchase_confirmation_workflow
-from . import test_ks_purchase_update_workflow
-from . import test_ks_purchase_cancel_workflow
-from . import test_ks_button_visibility
