f4be7c568784047f7ef2044bb8de0a89c5095c35
Ayush-Ksolves <ayush.shrivastava@ksolves.com>
2026-04-28 22:13:02 +0530

    Update _check_gst_no_format function

--- ks_contact/models/custom_partner.py
+++ ks_contact/models/custom_partner.py
@@ -180,7 +180,7 @@ class CustomContact(models.Model):
     def _check_gst_no_format(self):
         gst_pattern = re.compile(r'^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$')
         for rec in self:
-            if not rec.vat:
+            if not rec.vat or rec.parent_id:
                 continue
 
             gst = rec.vat.upper()
