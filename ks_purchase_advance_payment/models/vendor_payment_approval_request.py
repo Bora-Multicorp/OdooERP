a659995882bed9567a851440ef5f761068525427
Ayush-Ksolves <ayush.shrivastava@ksolves.com>
2026-04-30 22:32:18 +0530

    Remove the user error

--- ks_purchase_advance_payment/models/vendor_payment_approval_request.py
+++ ks_purchase_advance_payment/models/vendor_payment_approval_request.py
@@ -158,8 +158,6 @@ class VendorPaymentApprovalRequest(models.Model):
     def _check_total_amount_within_po_total(self):
         """Total amount_for_approval across all non-rejected requests for a PO must be less than the PO total."""
         for rec in self:
-            if rec.amount_for_approval:
-                raise ValidationError(_("Amount for Approval should be greater than 0."))
             if rec.state == 'rejected':
                 continue
             po = rec.purchase_order_id
