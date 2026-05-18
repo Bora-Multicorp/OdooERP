bcbdbaace69d74bcc91253264b84725579e0a7dd
Ayush-Ksolves <ayush.shrivastava@ksolves.com>
2026-05-18 19:45:39 +0530

    Update partner_visibility_control module

--- partner_visibility_control/__manifest__.py
+++ partner_visibility_control/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Partner Visibility Control',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     'category': 'Custom',
     'summary': (
         'Restrict partner_id dropdown in Sales and Purchase orders '
