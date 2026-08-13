1b56bbf14cf0da547f70509f93c75881c3768506
Anand Suthar <anand.suthar@sunarctechnologies.com>
2026-08-13 17:09:04 +0530

    latest purchase price in inventory

---
+++ bora_inventory_operations/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Bora Inventory Operations',
+    'version': '18.0.1.0.0',
+    'summary': 'Inventory operations custom enhancements including Latest Purchase Price field',
+    'category': 'Inventory/Inventory',
+    'author': 'Bora Multicorp',
+    'depends': [
+        'stock',
+        'purchase',
+    ],
+    'data': [
+        'views/product_product_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
