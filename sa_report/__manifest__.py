20e26395e9df38faf04156c88d0cf21fd4603efe
prakash.jain <prakash.jain@sunarctechnologies.com>
2026-08-17 17:09:39 +0530

    GRN recipt report added

---
+++ sa_report/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Sunarc Reports",
+    "summary": 'Custom Reports for Boratech Project',
+    "description": """
+        Sunarc Reports
+        ==============
+        Custom reports developed for the Boratech project.
+    """,
+    "author": "SunArc Technologies Private Limited",
+    "website": "https://sunarctechnologies.com/",
+    "category": "Reporting",
+    "version": "1.0",
+    "depends": ["stock"],
+    "data": [
+        "report/grn_report_template.xml",
+        "report/grn_report.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
