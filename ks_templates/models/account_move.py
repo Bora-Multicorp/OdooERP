63e3b3bc433b5deaf94c5fa7d352cd4dd8e299e3
Anand Suthar <anand.suthar@sunarctechnologies.com>
2026-09-19 12:52:56 +0530

    fix visibility

--- ks_templates/models/account_move.py
+++ ks_templates/models/account_move.py
@@ -31,15 +31,35 @@ class AccountMove(models.Model):
     ks_show_commercial_invoice = fields.Boolean(
         string='Show Commercial Invoice',
         compute='_compute_ks_commercial_invoice_visibility',
+        search='_search_ks_show_commercial_invoice',
         store=False
     )
     ks_show_domestic_tax_invoice = fields.Boolean(
         string='Show Domestic Tax Invoice',
         compute='_compute_ks_commercial_invoice_visibility',
+        search='_search_ks_show_domestic_tax_invoice',
         store=False
     )
 
-    @api.depends('invoice_line_ids.sale_line_ids.order_id.ks_zone')
+    def _search_ks_show_commercial_invoice(self, operator, value):
+        if operator in ('=', '!=') and isinstance(value, bool):
+            is_true = (operator == '=' and value) or (operator == '!=' and not value)
+            domain = [
+                ('company_id.country_id.code', '=', 'IN'),
+                '|',
+                ('partner_id.country_id.code', '=', 'IN'),
+                ('partner_id.commercial_partner_id.country_id.code', '=', 'IN')
+            ]
+            if is_true:
+                return ['!'] + domain
+            else:
+                return domain
+        return []
+
+    def _search_ks_show_domestic_tax_invoice(self, operator, value):
+        return []
+
+    @api.depends('invoice_line_ids.sale_line_ids')
     def _compute_ks_zone(self):
         for move in self:
             zone = ''
@@ -72,7 +92,7 @@ class AccountMove(models.Model):
             move.ks_is_company_indian = is_comp_indian
             move.ks_is_customer_indian = is_cust_indian
             move.ks_show_commercial_invoice = not (is_comp_indian and is_cust_indian)
-            move.ks_show_domestic_tax_invoice = is_comp_indian and is_cust_indian
+            move.ks_show_domestic_tax_invoice = True
 
 
     # Bank details for invoice (copied from sale order ks_bank_id when invoice is created from SO)
